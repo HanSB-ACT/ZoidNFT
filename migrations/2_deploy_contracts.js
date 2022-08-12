4f5c1cf8eee97db0eee88553b03548d2cebbcd08
HanSB-ACT <hansb@actgames.co.kr>
2022-08-12 16:38:04 +0900

    Fix Deploy Env

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,7 +5,7 @@ module.exports = function (deployer) {
     const ver = process.env.CONTRACT_VERSION;
     const name = process.env.CONTRACT_NAME;
     const symbol = process.env.CONTRACT_SYMBOL;
-    const baseUri = process.env.TOKEN_BASE_URL;
+    const baseUri = process.env.TOKEN_BASE_URI;
     const erc20 = process.env.ERC20_CONTRACT_ADDRESS;
     deployer.deploy(app, ver, name, symbol, baseUri, erc20);
 };
